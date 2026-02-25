6331170aaaab2ee3dd2f0bdaa5bf830c2ecdb3c6
igordoomofficial <igordoomofficial@gmail.com>
2026-02-25 14:19:40 -0300

    Fix JWT SECRET

--- backend/src/main/java/com/profilehub/security/JwtTokenProvider.java
+++ backend/src/main/java/com/profilehub/security/JwtTokenProvider.java
@@ -15,7 +15,7 @@ import javax.crypto.SecretKey;
 @Component
 public class JwtTokenProvider {
 
-    @Value("${app.jwtSecret:eW91ci1zZWNyZXQta2V5LWNoYW5nZS10aGlzLWluLXByb2R1Y3Rpb24tMTIzNDU2Nzg5MA==}")
+    @Value("${app.jwtSecret:TXlWZXJ5U2VjdXJlSldUU2VjcmV0S2V5MzJCeXRlc0xvbmdGb3JQcm9kdWN0aW9uVXNl}")
     private String jwtSecret;
 
     @Value("${app.jwtExpirationMs:86400000}")
